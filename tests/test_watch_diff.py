4bccdf025a5172f2e50041fe7818b3e2455a5e92
Francis Bergin <me@francisbergin.ca>
2018-12-31 14:56:51 -0500

    Add test for send_email method

--- tests/test_watch_diff.py
+++ tests/test_watch_diff.py
@@ -1,15 +1,21 @@
 """
 """
 
+import os
 import unittest
 
 import watch_diff
 
 
+smtp_host = os.environ.get('SMTP_HOST')
+smtp_port = os.environ.get('SMTP_PORT')
+smtp_user = os.environ.get('SMTP_USER')
+smtp_pass = os.environ.get('SMTP_PASS')
+
+
 class TestWatchDiff(unittest.TestCase):
 
     def test_api_available(self):
-
         self.assertTrue(watch_diff.Command)
         self.assertTrue(watch_diff.Diff)
         self.assertTrue(watch_diff.Email)
@@ -17,3 +23,11 @@ class TestWatchDiff(unittest.TestCase):
         self.assertTrue(watch_diff.ConsoleFormatter)
         self.assertTrue(watch_diff.HTMLFormatter)
         self.assertTrue(watch_diff.OutputFormatting)
+
+    @unittest.skipIf(not smtp_host, 'SMTP_HOST is not available')
+    @unittest.skipIf(not smtp_port, 'SMTP_PORT is not available')
+    @unittest.skipIf(not smtp_user, 'SMTP_USER is not available')
+    @unittest.skipIf(not smtp_pass, 'SMTP_PASS is not available')
+    def test_send_email(self):
+        e = watch_diff.Email(smtp_host, smtp_port, smtp_user, smtp_pass, 'watch-diff-tests', smtp_user)
+        e.send_email('watch diff tests', 'text', 'html')
